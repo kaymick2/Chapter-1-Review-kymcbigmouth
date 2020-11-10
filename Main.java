fc795fb5d997ffc1cb64704192b12d795361eb2f
Kyle McDowell <kymcd21@icstudents.org>
2020-11-10 04:32:04 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,26 @@
+class Main {
+  public static void main(String[] args) {
+    // 1.1
+    System.out.println("\n1.1");
+    System.out.println("***************");
+    System.out.println("3 + 4");
+    System.out.println(3 + 4);
+    System.out.println(3 + "4");
+    // 1.2
+    System.out.println("***************");
+    System.out.println("Section 1.2\n");
+    System.out.println("+----+\n|Kyle|\n+----+");
+    // 1.3 (I made two of them)
+    System.out.println("***************");
+    System.out.println("Section 1.3\n");
+    System.out.println("8888/\\8888/\\888\n8888\\/8888\\/888\n8888OO8888OO888\n8888O888888O888\n88888OOOOOO8888");
+    System.out.println("***************");
+    System.out.println("____/.\\___/.\\__\n____\\ /___\\ /__\n___O________O__\n____O______O___\n_____OOOOOO____");
+    // 1.4 (part of me seriously wants to try and define the sum of natural numbers,
+    // but it's 11:30 and I don't know what I'm doing.)
+    System.out.println("***************");
+    System.out.println("Section 1.4\n");
+    System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10);
+    System.out.println("***************");
+  }
+}
